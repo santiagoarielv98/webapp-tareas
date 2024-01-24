a4d93659f4633abab1b51e92c0756067b8d3af00
Santiago Ariel <santiagoarielv98@gmail.com>
2024-01-24 11:11:03 -0300

    refactorizacion

--- src/main/java/com/svillanueva/interceptors/TransactionalInterceptor.java
+++ src/main/java/com/svillanueva/interceptors/TransactionalInterceptor.java
@@ -1,13 +1,12 @@
 package com.svillanueva.interceptors;
 
+import com.svillanueva.services.ServiceJdbcException;
 import jakarta.inject.Inject;
 import jakarta.interceptor.AroundInvoke;
 import jakarta.interceptor.Interceptor;
 import jakarta.interceptor.InvocationContext;
 
 import java.sql.Connection;
-import java.util.logging.Logger;
-import com.svillanueva.services.ServiceJdbcException;
 
 @TransactionalJdbc
 @Interceptor
@@ -16,8 +15,6 @@ public class TransactionalInterceptor {
     @Inject
     private Connection conn;
 
-    @Inject
-    private Logger log;
 
     @AroundInvoke
     public Object transactional(InvocationContext invocation) throws Exception {
@@ -25,14 +22,10 @@ public class TransactionalInterceptor {
             conn.setAutoCommit(false);
         }
         try {
-            log.info(" ------> iniciando transaccion " + invocation.getMethod().getName() +
-                    " de la clase " + invocation.getMethod().getDeclaringClass());
             Object resultado = invocation.proceed();
             conn.commit();
-            log.info(" ------> realizando commit y finalizando transaccion " + invocation.getMethod().getName() +
-                    " de la clase " + invocation.getMethod().getDeclaringClass());
             return resultado;
-        } catch (ServiceJdbcException e){
+        } catch (ServiceJdbcException e) {
             conn.rollback();
             throw e;
         }
